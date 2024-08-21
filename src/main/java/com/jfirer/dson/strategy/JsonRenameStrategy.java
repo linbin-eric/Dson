50c3a64a909e55f199b025c6d5fe16d22fa63a0c
linbin <4961397@qq.com>
2024-08-21 10:49:50 +0800

    准备开发 Json 字段别名策略实现

---
+++ src/main/java/com/jfirer/dson/strategy/JsonRenameStrategy.java
@@ -0,0 +1,11 @@
+package com.jfirer.dson.strategy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+public interface JsonRenameStrategy
+{
+
+}
